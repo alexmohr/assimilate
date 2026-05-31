61bdaf76a9790bc186379a7c329085a5764842f7
Alexander Mohr <git@mohr.io>
2026-05-31 10:51:41 +0200

    feat: refactor schedule schema for multi-target support

--- crates/server/migrations/20260530130000_drop_schedule_unique_cron.sql
+++
@@ -1,4 +0,0 @@
--- SPDX-License-Identifier: Apache-2.0
--- SPDX-FileCopyrightText: 2026 Alexander Mohr
-
-ALTER TABLE schedules DROP CONSTRAINT IF EXISTS schedules_client_id_repo_id_cron_expression_key;
